08b0f18d261e798543fa310e9db4175fc999a52e
xeecos <flashindream@gmail.com>
2024-01-26 15:12:43 +0800

    add sdk

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-../luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ src/server.cpp -o build/server -O3 -std=c++11 -Wall -Wextra -pthread
-../luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ src/ipc_main.cpp -o build/ipc_main -O3 -std=c++14
-../luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ src/ipc_client.cpp -o build/ipc_client -O3 -std=c++14
-../luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ src/json_demo.cpp -o build/json_demo -O3 -std=c++11 -Wno-psabi
+arm-rockchip830-linux-uclibcgnueabihf-g++ src/server.cpp -o build/server -O3 -std=c++11 -Wall -Wextra -pthread
+arm-rockchip830-linux-uclibcgnueabihf-g++ src/ipc_main.cpp -o build/ipc_main -O3 -std=c++14
+arm-rockchip830-linux-uclibcgnueabihf-g++ src/ipc_client.cpp -o build/ipc_client -O3 -std=c++14
+arm-rockchip830-linux-uclibcgnueabihf-g++ src/json_demo.cpp -o build/json_demo -O3 -std=c++11 -Wno-psabi
